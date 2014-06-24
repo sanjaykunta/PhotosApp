34ee6277fc13e92a410803be2f24d678a8d4cb41
sanjaykunta <sanjaykumarkunta@gmail.com>
2014-06-23 23:05:49 -0500

    Initial Commit

---
+++ PhotosApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotosApp
+//
+//  Created by Sampath Kumar Kunta on 6/23/14.
+//  Copyright (c) 2014 Kent State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
